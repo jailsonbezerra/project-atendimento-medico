d8c2352ea6cf9cc7651c06e09d4722a8e7db28cc
Carol Montenegro <montenegroc189@gmail.com>
2025-07-02 11:45:04 -0300

    Update participantes.js

--- src/pages/home/participantes.js
+++ src/pages/home/participantes.js
@@ -6,6 +6,11 @@ export const participantes = [
     linkedin: 'https://www.linkedin.com/in/danilo-b-macedo-739423205/'
   },
   {
+    nome: 'Carol Montenegro',
+    github: 'https://github.com/montenegromc',
+    linkedin: 'https://www.linkedin.com/in/carol-montenegro'
+  },
+  {
     nome: 'Ciclano Souza',
     github: 'https://github.com/ciclano',
     linkedin: 'https://www.linkedin.com/in/ciclano/'
